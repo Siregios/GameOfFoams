c1515a73d70a67cf4fc45698dc81dc5720febea5
jfc3 <buckslice@gmail.com>
2017-08-10 18:37:37 -0700

    Redoing project structure

--- GameOfFoams/Assets/Enemies/Common/Death/DespawnDeath.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class DespawnDeath : MonoBehaviour, IDeathAction {
-    public void Die()
-    {
-        Destroy(this.transform.root.gameObject);
-    }
-}
--- GameOfFoams/Assets/Player/Game Logic/ControllerInput.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class ControllerInput : NullInput {
-
-	//Code this once I have access to a controller to test with
-}
