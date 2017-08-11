66ac46ebbccd06abf9a90fbb0958ca4f8338d622
jfc3 <buckslice@gmail.com>
2017-08-11 04:53:18 -0700

    Started basic boss scripts. Removed that coroutine library i just imported lol

--- Assets/Enemies/Bosses/BigBoss.cs
+++ Assets/Enemies/Bosses/BigBoss.cs
@@ -1,19 +1,31 @@
-﻿
-
-
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
 
 public class BigBoss : BossBase {
 
-	// Use this for initialization
-	protected override void Start () {
+    // Use this for initialization
+    protected override void Start() {
         base.Start();
 
+    }
+
+    Coroutine focusRoutine = null;
+
+    // Update is called once per frame
+    void Update() {
+        agent.destination = player.position;
+
+        if (agent.remainingDistance < agent.stoppingDistance) { // focus on player if close enough
+            if (focusRoutine == null) {
+                focusRoutine = StartCoroutine(FocusRoutine(player, 100.0f, agent.angularSpeed));
+            }
+        } else if (focusRoutine != null) {
+            StopCoroutine(focusRoutine);
+            focusRoutine = null;
+        }
 
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
 }
--- Assets/Enemies/Bosses/BossBase.cs
+++ Assets/Enemies/Bosses/BossBase.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-using MovementEffects;
 
 // base class for bosses with some helper functions
 public class BossBase : MonoBehaviour {
@@ -12,41 +11,41 @@ public class BossBase : MonoBehaviour {
     protected NavMeshAgent agent;
     protected int playerLayer;
 
-    // prob reference to player script
+    protected Transform player;
     //int playerLayer;
 
     protected virtual void Start() {
         //playerLayer = 1 << LayerMask.NameToLayer(Tags.Layers.Player);
 
+        GameObject playerGO = GameObject.FindGameObjectWithTag("Player");
+        if (playerGO) {
+            player = playerGO.transform;
+        } else {
+            Debug.LogWarning("No player found in boss script PROB GONNA BE ERRORS");
+        }
+
         source = GetComponent<AudioSource>();
         agent = GetComponent<NavMeshAgent>();
-        
+
     }
 
 
     // focus on a transforms position for an amount of time
     // turns at a rate degreesPerSec (not sure how well this works)
-    protected IEnumerator<float> FocusRoutine(Transform target, float time, float degreesPerSec = 360.0f) {
-        Quaternion startRot = transform.rotation;
-        float t = 0.0f;
-        Vector3 dir;
+    protected IEnumerator FocusRoutine(Transform target, float time, float degreesPerSec = 360.0f) {
         while (time > 0.0f) {
-            dir = (target.position - transform.position).normalized;
-            float angle = Vector3.Angle(dir, transform.forward);
+            Vector3 dir = (target.position - transform.position).normalized;
             Quaternion lookRot = Quaternion.LookRotation(new Vector3(dir.x, 0.0f, dir.z));
-            transform.rotation = Quaternion.Slerp(startRot, lookRot, t);
-            float r = (angle < 1.0f) ? degreesPerSec : degreesPerSec / angle;
-            t += r * Time.deltaTime;
-            time -= Time.deltaTime;
-            yield return Timing.WaitForOneFrame;
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRot, degreesPerSec * Time.deltaTime);
+            yield return 0;
         }
-        dir = (target.position - transform.position).normalized;
-        transform.rotation = Quaternion.LookRotation(new Vector3(dir.x, 0.0f, dir.z));
+        //dir = (target.position - transform.position).normalized;
+        //transform.rotation = Quaternion.LookRotation(new Vector3(dir.x, 0.0f, dir.z));
     }
 
     // look at a position at a rate of degreesPerSec
     // ends once you are looking at it
-    protected IEnumerator<float> LookAtRoutine(Vector3 target, float degreesPerSec = 360.0f) {
+    protected IEnumerator LookAtRoutine(Vector3 target, float degreesPerSec = 360.0f) {
         Quaternion startRot = transform.rotation;
         Vector3 dir = (target - transform.position).normalized;
         float angle = Vector3.Angle(dir, transform.forward);
@@ -56,7 +55,7 @@ public class BossBase : MonoBehaviour {
             transform.rotation = Quaternion.Slerp(startRot, lookRot, t);
             float r = (angle < 1.0f) ? degreesPerSec : degreesPerSec / angle;
             t += r * Time.deltaTime;
-            yield return Timing.WaitForOneFrame;
+            yield return 0;
         }
         transform.rotation = lookRot;
     }
