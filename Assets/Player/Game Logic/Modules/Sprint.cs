4c0ab66d86369106a08d616e8995629a670b7ce2
Ianmring <Ianmring@gmail.com>
2017-08-14 22:10:12 -0700

    Update Sprint.cs
    Added the logic used to make a stamina bar, should be easy to apply to UI

--- Assets/Player/Game Logic/Modules/Sprint.cs
+++ Assets/Player/Game Logic/Modules/Sprint.cs
@@ -5,7 +5,9 @@ public class Sprint : MonoBehaviour {
 
     [SerializeField]
     protected float speedMultiplier = 2;
-
+protected float Duration = 100;
+    protected float MaxDuration= 100;
+    //May need to playaround with the duration time in final build. 
     InputToMovement movement;
     IInput input;
 
@@ -17,13 +19,22 @@ public class Sprint : MonoBehaviour {
 
     void Update()
     {
-        if (input.sprinting)
+        if (input.sprinting && Duraiton > 0)
         {
             movement.speedMultiplier = speedMultiplier;
+            Duration - .1;
         }
+        if(Duration <= 0)
+        {
+        movemnt.speedMultiplyer = 1f;
+            Duration + 1;
+            //Again, may need a bit of playing around
+        }    
         else
         {
             movement.speedMultiplier = 1f;
         }
     }
 }
+
+//Edited on 8/14/2017 by Ian ring to add samina bar logic
