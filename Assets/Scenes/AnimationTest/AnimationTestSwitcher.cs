81f5f0383df5654beb2f08fcf0563ccc9d72ca8b
jfc3 <buckslice@gmail.com>
2018-01-09 18:07:58 -0800

    Reimported all animations and created test scene

---
+++ Assets/Scenes/AnimationTest/AnimationTestSwitcher.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTestSwitcher : MonoBehaviour {
+
+    public GameObject[] objects;
+    int activeObj = 0;
+    Camera activeCam;
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < objects.Length; ++i) {
+            objects[i].SetActive(i == 0);
+            objects[i].transform.GetChild(0).gameObject.SetActive(true);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            objects[activeObj].transform.GetChild(0).gameObject.SetActive(false);
+            objects[activeObj++].SetActive(false);
+            if (activeObj >= objects.Length) {
+                activeObj = 0;
+            }
+            objects[activeObj].transform.GetChild(0).gameObject.SetActive(true);
+            objects[activeObj].SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Application.Quit();
+        }
+	}
+}
